9abe74b94f4bc6c24c81cf0eb1e0969a02828e58
Dmitry <dimonch_rus@mail.ru>
2022-12-22 14:30:10 +0300

    add Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+Console.WriteLine();
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write($"{inputArray[i, j] + " ",3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeRowsFromMaxToMin(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inputArray.GetLength(1) - 1; k++)
+            {
+                if (inputArray[i, k] < inputArray[i, k + 1])
+                {
+                    int temp = inputArray[i, k];
+                    inputArray[i, k] = inputArray[i, k + 1];
+                    inputArray[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    return inputArray;
+}
+PrintArray(ChangeRowsFromMaxToMin(array));
